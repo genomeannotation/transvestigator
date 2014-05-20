9788bb0063e43cacbc3bbd49da12457b601d3e44
Brian Hall <bhall7@hawaii.edu>
2014-05-19 14:22:01 -1000

    added overlap() and nested() to transcript checker

--- src/transcript_checker.py
+++ src/transcript_checker.py
@@ -5,3 +5,24 @@ class TranscriptChecker:
     def __init__(self):
         pass
 
+    def overlap(self, indices1, indices2):
+        # Case 1:
+        #   indices1    --------
+        #   indices2  ------
+        if indices1[0] > indices2[0] and indices1[0] < indices2[1]:
+            return True
+        # Case 2:
+        #   indices1  --------
+        #   indices2      ------
+        elif indices1[1] > indices2[0] and indices1[1] < indices2[1]:
+            return True
+        else:
+            return False
+
+    def nested(self, indices1, indices2):
+        if indices1[0] >= indices2[0] and indices1[1] <= indices2[1]:
+            return True
+        elif indices2[0] >= indices1[0] and indices2[1] <= indices1[1]:
+            return True
+        else:
+            return False
--- test/transcript_checker_tests.py
+++ test/transcript_checker_tests.py
@@ -10,7 +10,44 @@ class TestTranscriptChecker(unittest.TestCase):
         self.checker = TranscriptChecker()
 
     def test_overlap(self):
-        pass
+        indices1 = [1, 10]
+        indices2 = [9, 15]
+        self.assertTrue(self.checker.overlap(indices1, indices2))
+
+    def test_overlap_returns_false_when_no_overlap(self):
+        indices1 = [1, 10]
+        indices2 = [11, 20]
+        self.assertFalse(self.checker.overlap(indices1, indices2))
+
+    def test_overlap_returns_false_when_nested(self):
+        indices1 = [1, 10]
+        indices2 = [2, 9]
+        self.assertFalse(self.checker.overlap(indices1, indices2))
+
+    def test_overlap_returns_false_when_identical(self): # not sure if this really happens
+        indices1 = [1, 10]
+        indices2 = [1, 10]
+        self.assertFalse(self.checker.overlap(indices1, indices2))
+
+    def test_nested(self):
+        indices1 = [1, 10]
+        indices2 = [2, 9]
+        self.assertTrue(self.checker.nested(indices1, indices2))
+
+    def test_nested_returns_false_when_overlapping(self):
+        indices1 = [1, 10]
+        indices2 = [9, 15]
+        self.assertFalse(self.checker.nested(indices1, indices2))
+
+    def test_nested_returns_true_when_identical(self):
+        indices1 = [1, 10]
+        indices2 = [1, 10]
+        self.assertTrue(self.checker.nested(indices1, indices2))
+
+    def test_nested_returns_false_when_separate(self):
+        indices1 = [1, 10]
+        indices2 = [11, 20]
+        self.assertFalse(self.checker.nested(indices1, indices2))
 
 
 ##########################
