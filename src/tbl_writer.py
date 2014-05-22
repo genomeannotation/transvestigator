66dbb31c9a7f4f694fdad91195b90196af178678
Theodore DeRego <tderego94@gmail.com>
2014-05-21 15:58:39 -1000

    gff_gene_to_tbl uses hasattr now

--- src/tbl_writer.py
+++ src/tbl_writer.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 
 def gff_gene_to_tbl(gff_gene):
-    if not gff_gene.has_child("mRNA"):
+    if not hasattr(gff_gene, "mrna"):
         raise Exception("can't write tbl entry for "+gff_gene.attributes["ID"]+" because it has no mRNAs")
     if len(gff_gene.mrna) > 1:
         raise Exception("can't write tbl entry for "+gff_gene.attributes["ID"]+" because it has multiple mRNAs")
-    if not gff_gene.mrna[0].has_child("CDS"):
+    if not hasattr(gff_gene.mrna[0], "cds"):
         raise Exception("can't write tbl entry for "+gff_gene.attributes["ID"]+" because its mRNA has no CDS")
+    # Check for starts and stops
+    has_start = False
+    if gff_gene.has_child("start_codon"):
+        has_start = True
+    # Create tbl entry
     tbl = ""
     tbl += "<"+str(gff_gene.start)+"\t>"+str(gff_gene.end)+"\tgene\n"
     tbl += "\t\t\tlocus_tag\t"+gff_gene.attributes["ID"]+"\n"
--- test/tbl_writer_tests.py
+++ test/tbl_writer_tests.py
@@ -12,7 +12,7 @@ class TestTblWriter(unittest.TestCase):
     def test_gff_gene_to_tbl_throws_on_no_mrnas(self):
         gff_gene = Mock()
         gff_gene.attributes = {"ID":"foo_gene"}
-        gff_gene.has_child = Mock(return_value=False)
+        del gff_gene.mrna
 
         thrown = False
         try:
@@ -20,13 +20,11 @@ class TestTblWriter(unittest.TestCase):
         except Exception as error:
             self.assertEquals(str(error), "can't write tbl entry for foo_gene because it has no mRNAs")
             thrown = True
-        gff_gene.has_child.assert_called_with("mRNA")
         self.assertTrue(thrown)
 
     def test_gff_gene_to_tbl_throws_on_multiple_mrnas(self):
         gff_gene = Mock()
         gff_gene.attributes = {"ID":"foo_gene"}
-        gff_gene.has_child = Mock(return_value=True)
         gff_gene.mrna = [Mock(), Mock()]
 
         thrown = False
@@ -40,9 +38,8 @@ class TestTblWriter(unittest.TestCase):
     def test_gff_gene_to_tbl_throws_on_no_cds(self):
         gff_gene = Mock()
         gff_gene.attributes = {"ID":"foo_gene"}
-        gff_gene.has_child = Mock(return_value=True)
         gff_gene.mrna = [Mock()]
-        gff_gene.mrna[0].has_child = Mock(return_value=False)
+        del gff_gene.mrna[0].cds
 
         thrown = False
         try:
@@ -50,7 +47,6 @@ class TestTblWriter(unittest.TestCase):
         except Exception as error:
             self.assertEquals(str(error), "can't write tbl entry for foo_gene because its mRNA has no CDS")
             thrown = True
-        gff_gene.mrna[0].has_child.assert_called_with("CDS")
         self.assertTrue(thrown)
 
     def test_gff_gene_to_tbl_nostart_nostop_nogenename(self):
@@ -58,6 +54,33 @@ class TestTblWriter(unittest.TestCase):
         "<1\t>100\tgene\n"\
         "\t\t\tlocus_tag\tfoo_gene\n"\
         "<1\t>100\tCDS\n"\
+        "\t\t\tprotein_id\tm.foo\n"
+
+        gff_gene = Mock()
+        gff_gene.start = 1
+        gff_gene.end = 100
+        gff_gene.attributes = {"ID":"foo_gene"}
+        
+        gff_mrna = Mock()
+        gff_mrna.start = 1
+        gff_mrna.end = 100
+        gff_mrna.attributes = {"ID":"m.foo"}
+
+        gff_cds = Mock()
+        gff_cds.start = 1 
+        gff_cds.end = 100
+        
+        gff_gene.mrna = [gff_mrna]
+        gff_mrna.cds = [gff_cds]
+
+        tbl = gff_gene_to_tbl(gff_gene)
+        self.assertEquals(tbl, expected)
+
+    def test_gff_gene_to_tbl_start_nostop_nogenename(self):
+        expected = \
+        "1\t>100\tgene\n"\
+        "\t\t\tlocus_tag\tfoo_gene\n"\
+        "1\t>100\tCDS\n"\
         "\t\t\tprotein_id\tm.foo\n"
 
         gff_gene = Mock()
