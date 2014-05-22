88f809f8100721a42c7257820f2174f30a0f2f08
Theodore DeRego <tderego94@gmail.com>
2014-05-21 16:30:47 -1000

    refactored gff_gene_to_tbl tests

--- test/tbl_writer_tests.py
+++ test/tbl_writer_tests.py
@@ -7,7 +7,22 @@ from src.tbl_writer import gff_gene_to_tbl
 class TestTblWriter(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.gff_gene = Mock()
+        self.gff_gene.start = 1
+        self.gff_gene.end = 100
+        self.gff_gene.attributes = {"ID":"foo_gene"}
+        
+        self.gff_mrna = Mock()
+        self.gff_mrna.start = 1
+        self.gff_mrna.end = 100
+        self.gff_mrna.attributes = {"ID":"m.foo"}
+
+        self.gff_cds = Mock()
+        self.gff_cds.start = 1 
+        self.gff_cds.end = 100
+        
+        self.gff_gene.mrna = [self.gff_mrna]
+        self.gff_mrna.cds = [self.gff_cds]
 
     def test_gff_gene_to_tbl_throws_on_no_mrnas(self):
         gff_gene = Mock()
@@ -56,26 +71,10 @@ class TestTblWriter(unittest.TestCase):
         "<1\t>100\tCDS\n"\
         "\t\t\tprotein_id\tm.foo\n"
 
-        gff_gene = Mock()
-        gff_gene.start = 1
-        gff_gene.end = 100
-        gff_gene.attributes = {"ID":"foo_gene"}
-        
-        gff_mrna = Mock()
-        gff_mrna.start = 1
-        gff_mrna.end = 100
-        gff_mrna.attributes = {"ID":"m.foo"}
+        del self.gff_mrna.start_codon
+        del self.gff_mrna.stop_codon
 
-        gff_cds = Mock()
-        gff_cds.start = 1 
-        gff_cds.end = 100
-        
-        gff_gene.mrna = [gff_mrna]
-        gff_mrna.cds = [gff_cds]
-        del gff_mrna.start_codon
-        del gff_mrna.stop_codon
-
-        tbl = gff_gene_to_tbl(gff_gene)
+        tbl = gff_gene_to_tbl(self.gff_gene)
         self.assertEquals(tbl, expected)
 
     def test_gff_gene_to_tbl_start_nostop_nogenename(self):
@@ -85,26 +84,10 @@ class TestTblWriter(unittest.TestCase):
         "1\t>100\tCDS\n"\
         "\t\t\tprotein_id\tm.foo\n"
 
-        gff_gene = Mock()
-        gff_gene.start = 1
-        gff_gene.end = 100
-        gff_gene.attributes = {"ID":"foo_gene"}
-        
-        gff_mrna = Mock()
-        gff_mrna.start = 1
-        gff_mrna.end = 100
-        gff_mrna.attributes = {"ID":"m.foo"}
+        self.gff_mrna.start_codon = [Mock()]
+        del self.gff_mrna.stop_codon
 
-        gff_cds = Mock()
-        gff_cds.start = 1 
-        gff_cds.end = 100
-        
-        gff_gene.mrna = [gff_mrna]
-        gff_mrna.cds = [gff_cds]
-        gff_mrna.start_codon = [Mock()]
-        del gff_mrna.stop_codon
-
-        tbl = gff_gene_to_tbl(gff_gene)
+        tbl = gff_gene_to_tbl(self.gff_gene)
         self.assertEquals(tbl, expected)
 
     def test_gff_gene_to_tbl_nostart_stop_nogenename(self):
@@ -114,26 +97,10 @@ class TestTblWriter(unittest.TestCase):
         "<1\t100\tCDS\n"\
         "\t\t\tprotein_id\tm.foo\n"
 
-        gff_gene = Mock()
-        gff_gene.start = 1
-        gff_gene.end = 100
-        gff_gene.attributes = {"ID":"foo_gene"}
-        
-        gff_mrna = Mock()
-        gff_mrna.start = 1
-        gff_mrna.end = 100
-        gff_mrna.attributes = {"ID":"m.foo"}
+        del self.gff_mrna.start_codon
+        self.gff_mrna.stop_codon = [Mock()]
 
-        gff_cds = Mock()
-        gff_cds.start = 1 
-        gff_cds.end = 100
-        
-        gff_gene.mrna = [gff_mrna]
-        gff_mrna.cds = [gff_cds]
-        del gff_mrna.start_codon
-        gff_mrna.stop_codon = [Mock()]
-
-        tbl = gff_gene_to_tbl(gff_gene)
+        tbl = gff_gene_to_tbl(self.gff_gene)
         self.assertEquals(tbl, expected)
 
     def test_gff_gene_to_tbl_start_stop_nogenename(self):
@@ -143,26 +110,10 @@ class TestTblWriter(unittest.TestCase):
         "1\t100\tCDS\n"\
         "\t\t\tprotein_id\tm.foo\n"
 
-        gff_gene = Mock()
-        gff_gene.start = 1
-        gff_gene.end = 100
-        gff_gene.attributes = {"ID":"foo_gene"}
-        
-        gff_mrna = Mock()
-        gff_mrna.start = 1
-        gff_mrna.end = 100
-        gff_mrna.attributes = {"ID":"m.foo"}
+        self.gff_mrna.start_codon = [Mock()]
+        self.gff_mrna.stop_codon = [Mock()]
 
-        gff_cds = Mock()
-        gff_cds.start = 1 
-        gff_cds.end = 100
-        
-        gff_gene.mrna = [gff_mrna]
-        gff_mrna.cds = [gff_cds]
-        gff_mrna.start_codon = [Mock()]
-        gff_mrna.stop_codon = [Mock()]
-
-        tbl = gff_gene_to_tbl(gff_gene)
+        tbl = gff_gene_to_tbl(self.gff_gene)
         self.assertEquals(tbl, expected)
 
 
