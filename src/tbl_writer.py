a1014f760e60a57f25be1fc6e4d0b9df29d4fd6a
Theodore DeRego <tderego94@gmail.com>
2014-05-21 17:07:08 -1000

    transcript_to_tbl

--- src/tbl_writer.py
+++ src/tbl_writer.py
@@ -31,3 +31,15 @@ def gff_gene_to_tbl(gff_gene):
     tbl += str(gff_gene.mrna[0].cds[0].end)+"\tCDS\n"
     tbl += "\t\t\tprotein_id\t"+gff_gene.mrna[0].attributes["ID"]+"\n"
     return tbl
+
+def transcript_to_tbl(transcript):
+    if not transcript.genes: # No genes
+        raise Exception("can't write tbl entry for "+transcript.sequence.header+" because it has no genes")
+    if len(transcript.genes) > 1:
+        raise Exception("can't write tbl entry for "+transcript.sequence.header+" because it has multiple genes")
+    tbl = ""
+    tbl += ">Feature "+transcript.sequence.header+"\n"
+    tbl += "1\t"+str(len(transcript.sequence.bases))+"\tREFERENCE\n"
+    tbl += "\t\t\tNCBI\t12345\n"
+    tbl += gff_gene_to_tbl(transcript.genes[0])
+    return tbl
--- test/tbl_writer_tests.py
+++ test/tbl_writer_tests.py
@@ -2,27 +2,44 @@
 
 import unittest
 from mock import Mock
-from src.tbl_writer import gff_gene_to_tbl
+from src.tbl_writer import gff_gene_to_tbl, transcript_to_tbl
 
 class TestTblWriter(unittest.TestCase):
 
     def setUp(self):
-        self.gff_gene = Mock()
-        self.gff_gene.start = 1
-        self.gff_gene.end = 100
-        self.gff_gene.attributes = {"ID":"foo_gene"}
+        self.gff_gene0 = Mock()
+        self.gff_gene0.start = 1
+        self.gff_gene0.end = 100
+        self.gff_gene0.attributes = {"ID":"foo_gene"}
         
-        self.gff_mrna = Mock()
-        self.gff_mrna.start = 1
-        self.gff_mrna.end = 100
-        self.gff_mrna.attributes = {"ID":"m.foo"}
+        self.gff_mrna0 = Mock()
+        self.gff_mrna0.start = 1
+        self.gff_mrna0.end = 100
+        self.gff_mrna0.attributes = {"ID":"m.foo"}
 
-        self.gff_cds = Mock()
-        self.gff_cds.start = 1 
-        self.gff_cds.end = 100
+        self.gff_cds0 = Mock()
+        self.gff_cds0.start = 1 
+        self.gff_cds0.end = 100
         
-        self.gff_gene.mrna = [self.gff_mrna]
-        self.gff_mrna.cds = [self.gff_cds]
+        self.gff_gene0.mrna = [self.gff_mrna0]
+        self.gff_mrna0.cds = [self.gff_cds0]
+
+        self.gff_gene1 = Mock()
+        self.gff_gene1.start = 42
+        self.gff_gene1.end = 80
+        self.gff_gene1.attributes = {"ID":"foo_gene"}
+        
+        self.gff_mrna1 = Mock()
+        self.gff_mrna1.start = 42 
+        self.gff_mrna1.end = 80
+        self.gff_mrna1.attributes = {"ID":"m.foo"}
+
+        self.gff_cds1 = Mock()
+        self.gff_cds1.start = 42 
+        self.gff_cds1.end = 80
+        
+        self.gff_gene1.mrna = [self.gff_mrna1]
+        self.gff_mrna1.cds = [self.gff_cds1]
 
     def test_gff_gene_to_tbl_throws_on_no_mrnas(self):
         gff_gene = Mock()
@@ -71,10 +88,10 @@ class TestTblWriter(unittest.TestCase):
         "<1\t>100\tCDS\n"\
         "\t\t\tprotein_id\tm.foo\n"
 
-        del self.gff_mrna.start_codon
-        del self.gff_mrna.stop_codon
+        del self.gff_mrna0.start_codon
+        del self.gff_mrna0.stop_codon
 
-        tbl = gff_gene_to_tbl(self.gff_gene)
+        tbl = gff_gene_to_tbl(self.gff_gene0)
         self.assertEquals(tbl, expected)
 
     def test_gff_gene_to_tbl_start_nostop_nogenename(self):
@@ -84,10 +101,10 @@ class TestTblWriter(unittest.TestCase):
         "1\t>100\tCDS\n"\
         "\t\t\tprotein_id\tm.foo\n"
 
-        self.gff_mrna.start_codon = [Mock()]
-        del self.gff_mrna.stop_codon
+        self.gff_mrna0.start_codon = [Mock()]
+        del self.gff_mrna0.stop_codon
 
-        tbl = gff_gene_to_tbl(self.gff_gene)
+        tbl = gff_gene_to_tbl(self.gff_gene0)
         self.assertEquals(tbl, expected)
 
     def test_gff_gene_to_tbl_nostart_stop_nogenename(self):
@@ -97,10 +114,10 @@ class TestTblWriter(unittest.TestCase):
         "<1\t100\tCDS\n"\
         "\t\t\tprotein_id\tm.foo\n"
 
-        del self.gff_mrna.start_codon
-        self.gff_mrna.stop_codon = [Mock()]
+        del self.gff_mrna0.start_codon
+        self.gff_mrna0.stop_codon = [Mock()]
 
-        tbl = gff_gene_to_tbl(self.gff_gene)
+        tbl = gff_gene_to_tbl(self.gff_gene0)
         self.assertEquals(tbl, expected)
 
     def test_gff_gene_to_tbl_start_stop_nogenename(self):
@@ -110,10 +127,55 @@ class TestTblWriter(unittest.TestCase):
         "1\t100\tCDS\n"\
         "\t\t\tprotein_id\tm.foo\n"
 
-        self.gff_mrna.start_codon = [Mock()]
-        self.gff_mrna.stop_codon = [Mock()]
+        self.gff_mrna0.start_codon = [Mock()]
+        self.gff_mrna0.stop_codon = [Mock()]
 
-        tbl = gff_gene_to_tbl(self.gff_gene)
+        tbl = gff_gene_to_tbl(self.gff_gene0)
+        self.assertEquals(tbl, expected)
+
+    def test_transcript_to_tbl_throws_on_no_genes(self):
+        transcript = Mock()
+        transcript.genes = [] 
+        transcript.sequence.header = "foo_seq"
+
+        thrown = False
+        try:
+            tbl = transcript_to_tbl(transcript)
+        except Exception as error:
+            self.assertEquals(str(error), "can't write tbl entry for foo_seq because it has no genes")
+            thrown = True
+        self.assertTrue(thrown)
+
+    def test_transcript_to_tbl_throws_on_multiple_genes(self):
+        transcript = Mock()
+        transcript.genes = [self.gff_gene0, self.gff_gene1] 
+        transcript.sequence.header = "foo_seq"
+
+        thrown = False
+        try:
+            tbl = transcript_to_tbl(transcript)
+        except Exception as error:
+            self.assertEquals(str(error), "can't write tbl entry for foo_seq because it has multiple genes")
+            thrown = True
+        self.assertTrue(thrown)
+
+    def test_transcript_to_tbl(self):
+        expected = \
+        ">Feature foo_seq\n"\
+        "1\t4\tREFERENCE\n"\
+        "\t\t\tNCBI\t12345\n"\
+        "1\t100\tgene\n"\
+        "\t\t\tlocus_tag\tfoo_gene\n"\
+        "1\t100\tCDS\n"\
+        "\t\t\tprotein_id\tm.foo\n"
+
+        transcript = Mock()
+        transcript.genes = [self.gff_gene0] 
+        transcript.sequence = Mock()
+        transcript.sequence.header = "foo_seq"
+        transcript.sequence.bases = "ATGC"
+
+        tbl = transcript_to_tbl(transcript)
         self.assertEquals(tbl, expected)
 
 
