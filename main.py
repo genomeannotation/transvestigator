df655860b60711872009031f8b3a733bd6b8420f
Brian Hall <bhall7@hawaii.edu>
2014-05-22 15:07:44 -1000

    started mainifying main.py

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ from src.gff_reader import read_gff
 from src.gff_feature import GFFFeature
 from src.transcript_checker import TranscriptChecker
 
+gffpath = "foo.gff"
+annopath = "foo.anno"
+
 def main():
-    with open("foo.gff", "r") as gfffile:
+    with open(gffpath, "r") as gfffile:
         gff = read_gff(gfffile)
     if not gff:
         exit()
-    checker = TranscriptChecker()
-    checker.sort_genes(gff)
-    for transcript, geneslist in checker.transcripts.items():
-        print(transcript + ": " + str(len(geneslist)))
+    with open(annopath, "r") as annofile:
+        annos = read_ipr(annofile)
+
 
 
 ###########################################
