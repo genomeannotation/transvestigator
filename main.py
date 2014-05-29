9918c7f9e65a7a73331158c7a65a06a2236d21b6
Theodore DeRego <tderego94@gmail.com>
2014-05-28 16:56:29 -1000

    fixed main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
-from src.gff import read_gff, annotate_gff
-from src.fasta import read_fasta
-from src.annotation_extractor import read_ipr
-from src.transcript_checker import create_starts_and_stops
+from src.gff import read_gff, annotate_genes
+from src.sequence import read_fasta
+from src.ipr import read_ipr
 from src.transcript_builder import build_transcript_dictionary
 from src.transcript_fixer import fix_transcript
-from src.tbl_writer import transcript_to_tbl
 
 fastapath = "foo.fasta"
 gffpath = "foo.gff"
@@ -27,13 +25,13 @@ def main():
         annos = read_ipr(annofile)
     if not annos:
         exit() # TODO maybe no annos is okay...
-    annotate_gff(gff, annos)
+    annotate_genes(gff.gene, annos)
     transcript_dict = build_transcript_dictionary(seqs, gff.gene)
     with open(tblpath, "w") as tblfile:
         for transcript in transcript_dict.values():
-            create_starts_and_stops(transcript)
+            transcript.create_starts_and_stops()
             fix_transcript(transcript)
-            tblfile.write(transcript_to_tbl(transcript))
+            tblfile.write(transcript.to_tbl())
 
 
 
