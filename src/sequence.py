f1487bcd05b2a92040216f7dbd941f5a58b95db7
Brian Hall <bhall7@hawaii.edu>
2014-05-28 15:40:04 -1000

    moved read_fasta to sequence.py

--- src/sequence.py
+++ src/sequence.py
@@ -12,3 +12,22 @@ class Sequence:
         result += self.bases + '\n'
         return result
 
+####################################################
+
+def read_fasta(io_buffer):
+    header = ''
+    bases = ''
+    seqs = {}
+    for line in io_buffer:
+        if line[0] == '>':
+            if len(header) > 0:
+                # Save the data
+                seqs[header] = Sequence(header, bases)
+            header = line[1:].strip().split()[0] # Get the next header
+            bases = ''
+        else:
+            bases += line.strip()
+    # Add the last sequence
+    seqs[header] = Sequence(header, bases)
+    return seqs
+
--- test/sequence_tests.py
+++ test/sequence_tests.py
@@ -12,3 +12,26 @@ class TestSequence(unittest.TestCase):
         expected = ">foo_seq\nGATTACA\n"
         self.assertEquals(expected, fasta)
 
+    def test_get_subsequence(self):
+        bases = "GATTACA"
+        subseq = get_subsequence(bases, 2, 4)
+        self.assertEquals("ATT", subseq)
+
+    def test_read_fasta_with_line_breaks(self):
+        line_breaks = io.StringIO('>seq_1\nGATTACAGATTACAGATTACAGATTACA\nGATTACAGATTACAGATTACAGATTACA\n' +
+                                 '>seq_2\nNNNNNNNNGATTACAGATTACAGATTAC\nANNNNNNNNNNN')
+
+        seqs = read_fasta(line_breaks)
+        self.assertEquals(2, len(seqs))
+        self.assertEquals('NNNNNNNNGATTACAGATTACAGATTACANNNNNNNNNNN', seqs["seq_2"].bases)
+
+    def test_read_fasta_without_line_breaks(self):
+        no_line_breaks = io.StringIO('>seq_1\nGATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGATTACA\n' +
+                                    '>seq_2\nNNNNNNNNGATTACAGATTACAGATTACANNNNNNNNNNN')
+        seqs = read_fasta(no_line_breaks)
+        self.assertEquals(2, len(seqs))
+        self.assertEquals('seq_1', seqs["seq_1"].header)
+        self.assertEquals('GATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGATTACA', seqs["seq_1"].bases)
+        self.assertEquals('seq_2', seqs["seq_2"].header)
+        self.assertEquals('NNNNNNNNGATTACAGATTACAGATTACANNNNNNNNNNN', seqs["seq_2"].bases)
+        
