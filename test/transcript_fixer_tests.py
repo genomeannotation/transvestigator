0eb2f8651bdaf142473f7d27bd7219b87626620b
Brian Hall <bhall7@hawaii.edu>
2014-06-19 11:03:01 -1000

    added test fixture to transcript fixer tests

--- test/transcript_fixer_tests.py
+++ test/transcript_fixer_tests.py
@@ -4,28 +4,32 @@ from src.transcript_fixer import fix_transcript
 
 class TestTranscriptFixer(unittest.TestCase):
 
+    def setUp(self):
+        self.transcript = Mock()
+        self.gene0 = Mock()
+        self.gene1 = Mock()
+
+        self.mrna0 = Mock()
+        self.mrna1 = Mock()
+
+        self.cds0 = Mock()
+        self.cds0.length = Mock(return_value=5)
+        self.cds1 = Mock()
+        self.cds1.length = Mock(return_value=6)
+
+        self.transcript.genes = [self.gene0, self.gene1]
+        self.gene0.mrna = [self.mrna0]
+        self.gene1.mrna = [self.mrna1]
+        self.mrna0.cds = [self.cds0]
+        self.mrna1.cds = [self.cds1]
+
     def test_fix_transcript(self):
+        fix_transcript(self.transcript)
+        self.assertEquals(self.transcript.genes, [self.gene1])
+
+    def test_fix_phase(self):
         transcript = Mock()
-        gene0 = Mock()
-        gene1 = Mock()
 
-        mrna0 = Mock()
-        mrna1 = Mock()
-
-        cds0 = Mock()
-        cds0.length = Mock(return_value=5)
-        cds1 = Mock()
-        cds1.length = Mock(return_value=6)
-
-        transcript.genes = [gene0, gene1]
-        gene0.mrna = [mrna0]
-        gene1.mrna = [mrna1]
-        mrna0.cds = [cds0]
-        mrna1.cds = [cds1]
-
-        fix_transcript(transcript)
-
-        self.assertEquals(transcript.genes, [gene1])
 
 
 ###################
