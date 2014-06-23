6959c605e7313f6ecf1f8767c468f6a4e539ae1e
Brian Hall <bhall7@hawaii.edu>
2014-06-23 12:31:23 -1000

    refined interface; annos and blacklist optional

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
+import os
 import sys
 from src.gff import read_gff 
 from src.annotation import read_annotations, annotate_genes
@@ -8,9 +9,10 @@ from src.sequence import read_fasta
 from src.transcript_builder import build_transcript_dictionary
 from src.transcript_fixer import fix_transcript, fix_phase
 
-fastapath = "transcriptome.fasta"
-gffpath = "transcriptome.gff"
-annopath = "transcriptome.anno"
+fastapath = "transcriptome.fasta"  # Required
+gffpath = "transcriptome.gff"  # Required
+annopath = "transcriptome.anno"  # Optional
+blacklistpath = "transcriptome.blacklist"  # Optional
 tblpath = "transcriptome.new.tbl"
 outfastapath = "transcriptome.new.fsa"
 
@@ -24,6 +26,8 @@ def read_transcript_blacklist(io_buffer):
 
 def main():
     path = ""
+    annos = None
+    transcript_blacklist = None
     if len(sys.argv) > 1:
         path = sys.argv[1] + "/"
     sys.stderr.write("Reading files ... ")
@@ -37,27 +41,31 @@ def main():
     if not gff:
         sys.stderr.write("Error reading gff; exiting now\n")
         exit()
-    with open(path + annopath, "r") as annofile:
-        annos = read_annotations(annofile)
-    if not annos:
-        sys.stderr.write("Error reading annotations; exiting now\n")
-        exit() # TODO maybe no annos is okay...
-    with open(path + "transcript_blacklist", "r") as transcript_blacklist_file:
-        transcript_blacklist = read_transcript_blacklist(transcript_blacklist_file)
-    if not transcript_blacklist:
-        sys.stderr.write("Error reading blacklist; exiting now\n")
-        exit()
+    if verify_path(path + annopath):
+        with open(path + annopath, "r") as annofile:
+            annos = read_annotations(annofile)
+        if not annos:
+            sys.stderr.write("Error reading annotations; exiting now\n")
+            exit() 
+    if verify_path(blacklistpath):
+        with open(path + blacklistpath, "r") as transcript_blacklist_file:
+            transcript_blacklist = read_transcript_blacklist(transcript_blacklist_file)
+        if not transcript_blacklist:
+            sys.stderr.write("Error reading blacklist; exiting now\n")
+            exit()
     sys.stderr.write("done.\n\n")
-    sys.stderr.write("Annotating genes ... ")
-    annotate_genes(gff.gene, annos)
-    sys.stderr.write("done.\n\n")
+    if annos:
+        sys.stderr.write("Annotating genes ... ")
+        annotate_genes(gff.gene, annos)
+        sys.stderr.write("done.\n\n")
     sys.stderr.write("Mapping gff data to transcripts ... ")
     transcript_dict = build_transcript_dictionary(seqs, gff.gene)
     sys.stderr.write("done.\n\n")
     sys.stderr.write("Writing .tbl file ... ")
     with open(tblpath, "w") as tblfile:
         for transcript in transcript_dict.values():
-            if transcript.sequence.header in transcript_blacklist:
+            if transcript_blacklist and\
+                    transcript.sequence.header in transcript_blacklist:
                 continue
             transcript.fix_feature_lengths()
             transcript.make_positive()
@@ -69,10 +77,17 @@ def main():
     sys.stderr.write("Writing .fsa file ... ")
     with open(outfastapath, "w") as outfastafile:
         for transcript in transcript_dict.values():
-            if transcript.sequence.header in transcript_blacklist:
+            if transcript_blacklist and\
+                    transcript.sequence.header in transcript_blacklist:
                 continue
             outfastafile.write(transcript.sequence.to_fasta())
     sys.stderr.write("done.\n\n")
+
+
+def verify_path(path):
+    if not os.path.isfile(path):
+        return False
+    return True
 
 
 ###########################################
