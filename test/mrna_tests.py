fc59f610bd9a37373eaa5264d56eedee127150ba
Theodore DeRego <tderego94@gmail.com>
2014-11-21 14:11:47 -1000

    mrna.make_positive test

--- test/mrna_tests.py
+++ test/mrna_tests.py
@@ -9,15 +9,47 @@ class TestMrna(unittest.TestCase):
         gff_mrna = Mock()
         gff_mrna.type = "mRNA"
         
-        tran_mrna = Mrna.from_gff_feature(gff_mrna)
-        self.assertTrue(tran_mrna)
+        mrna = Mrna.from_gff_feature(gff_mrna)
+        self.assertTrue(mrna)
 
     def test_from_gff_features_fails(self):
         gff_mrna = Mock()
         gff_mrna.type = "asdf"
         
-        tran_mrna = Mrna.from_gff_feature(gff_mrna)
-        self.assertFalse(tran_mrna)
+        mrna = Mrna.from_gff_feature(gff_mrna)
+        self.assertFalse(mrna)
+
+    #### MAKE POSITIVE TESTS ####
+
+    def test_make_positive(self):
+        seq_len = 8
+
+        mrna = Mrna(start=1, end=7, strand='-')
+        cds = Mock()
+        cds.type = 'cds'
+        cds.start = 1
+        cds.end = 7
+        cds.strand = '-'
+        exon = Mock()
+        exon.type = 'exon'
+        exon.start = 1
+        exon.end = 7
+        cds.strand = '-'
+
+        mrna.add_child(cds)
+        mrna.add_child(exon)
+
+        mrna.make_positive(seq_len)
+
+        self.assertEqual(mrna.start, 2)
+        self.assertEqual(mrna.end, 8)
+        self.assertEqual(mrna.strand, '+')
+        self.assertEqual(cds.start, 2)
+        self.assertEqual(cds.end, 8)
+        self.assertEqual(cds.strand, '+')
+        self.assertEqual(exon.start, 2)
+        self.assertEqual(exon.end, 8)
+        self.assertEqual(exon.strand, '+')
 
 
 ###################
