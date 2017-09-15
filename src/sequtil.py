926359f4227973c8f934da78799862553b1bfd48
Woods26 <forest.bremer@gmail.com>
2017-09-15 12:36:22 -1000

    Simplified chained comparisons

--- src/sequtil.py
+++ src/sequtil.py
@@ -66,12 +66,12 @@ def overlap(indices1, indices2):
     # Case 1:
     #   indices1    --------
     #   indices2  ------
-    if indices1[0] > indices2[0] and indices1[0] <= indices2[1]:
+    if indices2[0] < indices1[0] <= indices2[1]:
         return True
     # Case 2:
     #   indices1  --------
     #   indices2      ------
-    elif indices1[1] >= indices2[0] and indices1[1] < indices2[1]:
+    elif indices2[0] <= indices1[1] < indices2[1]:
         return True
     else:
         return False
